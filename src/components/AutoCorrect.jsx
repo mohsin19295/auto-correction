0da12ebf8a9c9e28504e1e186446e9193cd952de
mohsin19295 <mohsin19295@gmail.com>
2022-07-17 18:37:18 +0530

    basic app complete

--- src/components/AutoCorrect.jsx
+++ src/components/AutoCorrect.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const data = {
+  moshin: "Mohsin",
+  masaai: "Masai",
+  devloper: "developer",
+  github: "GitHub",
+  coding: "codding",
+  react: "React",
+    };
 
 const AutoCorrect = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [text, setText] = useState('')
+
+    function checkForWordMatch(e) {
+       
+      if (e[e.length - 1] === " ") {
+        const wordArr = e.split(" "); 
+        const word = wordArr[wordArr.length - 2]; 
+   
+          const convertToCorrect = data[word]; 
+          
+        if (convertToCorrect) { 
+          wordArr[wordArr.length - 2] = convertToCorrect; 
+        }
+        e = wordArr.join(" "); 
+      }
+      setText(e);
+    }
+    return (
+      <>
+     
+        <textarea
+          value={text}
+          onChange={(e) => {
+            checkForWordMatch(e.target.value);
+          }}
+        />
+      </>
+    );
 }
 
 export default AutoCorrect
