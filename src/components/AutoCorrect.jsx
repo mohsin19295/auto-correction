fecb8c3617818394db1f4cd652aa264ec417860e
mohsin19295 <mohsin19295@gmail.com>
2022-07-17 18:41:48 +0530

    info added

--- src/components/AutoCorrect.jsx
+++ src/components/AutoCorrect.jsx
@@ -1,4 +1,14 @@
 import React, { useState } from 'react'
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  width: 500px;
+  height: 20vh;
+  outline: none;
+  background-color: lightGrey;
+  font-size: 1.2rem;
+  margin-top: 1rem;
+`;
 
 const data = {
   moshin: "Mohsin",
@@ -13,8 +23,8 @@ const AutoCorrect = () => {
     const [text, setText] = useState('')
 
     function checkForWordMatch(e) {
-       
       if (e[e.length - 1] === " ") {
+       
         const wordArr = e.split(" "); 
         const word = wordArr[wordArr.length - 2]; 
    
@@ -25,16 +35,23 @@ const AutoCorrect = () => {
         }
         e = wordArr.join(" "); 
       }
-      setText(e);
+      setText(e); 
     }
     return (
       <>
-     
-        <textarea
+        <h3>
+          Once you type a word that is in our data with the same misspelling,{" "}
+          <br /> it will automatically correct that word after passing the
+          space.
+        </h3>
+
+
+        <TextArea
           value={text}
+          placeholder = "Type here"
           onChange={(e) => {
-            checkForWordMatch(e.target.value);
-          }}
+              checkForWordMatch(e.target.value);
+            }}
         />
       </>
     );
